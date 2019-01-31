c4aa1994fa9e33de92528219a5a6c4ae736a0ae4
D13EG04L3J4NDR0 <alejandrohuertas1992@gmail.com>
2019-01-30 22:02:01 -0500

    Primer Cambio

---
+++ Sistema/Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        [Required (ErrorMessage = "Campo Requerido")]
+        [StringLength (50, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 a 50 Caracteres")]
+        public String Nombre { get; set;}
+        [StringLength(256, ErrorMessage = "La dscripcion no debe exceder 256 caracteres")]
+        [Display (Name = "Descripción")]
+        public String Descripcion { get; set; }
+        public Boolean Estado { get; set; }
+
+
+
+    }
+}
